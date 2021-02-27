db5d326ef7569927b06521c836b7c0c5153bac10
Chiarucci Damiano <chiarucci@SVS041P20S.homenet.telecomitalia.it>
2021-02-27 10:39:47 +0100

    svg-transfromer

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import { Image } from 'react-native-elements';
 import { SvgCssUri }  from 'react-native-svg';
+import Homer from '../assets/homer.svg';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import homerImg from '../assets/homer-simpson.jpg';
 
@@ -15,6 +16,7 @@ const HomeScreen = ({ navigation }) => {
     console.log(navigation)
     return (
         <View style={{flex: 1}}>
+            <Homer width={400} height={500} />
             <Image PlaceholderContent={<ActivityIndicator/>} style={{height: 100, width: 300, resizeMode: "cover"}} source={{uri: "https://images.unsplash.com/photo-1543373014-cfe4f4bc1cdf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3604&q=80"}}/>
             <SvgCssUri
                 width="100%"
